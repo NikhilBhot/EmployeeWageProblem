4ca845389e46c80070e2d853edc0492c26369166
unknown <bhotnikhil25@gmail.com>
2022-12-03 21:46:38 +0530

    [Nikhil]-Add

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EmpPartTimeWages
+{
+    
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                int IS_FULL_TIME = 1;
+                int IS_PART_TIME = 2;
+                int IS_ABSENT = 0;
+                int EMP_RATE_PER_HOUR = 20;
+
+                int empHrs = 0;
+                int empWage = 0;
+
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 3);
+                if (empCheck == IS_FULL_TIME)
+                {
+                    Console.WriteLine("Employee Is Present");
+                    empHrs = 8;
+
+                }
+                else if (empCheck == IS_PART_TIME) 
+                {
+                    Console.WriteLine("Employee is Part Time");
+                    empHrs = 4;
+                }
+                else
+                {
+                    Console.WriteLine("Employee Is Absent");
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Employee Wage:" + empWage);
+            }
+        }
+    }
+
